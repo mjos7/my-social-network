ecfe10f9255c13a87e9d524017899e3b0580993b
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-10-03 22:33:00 -0700

    fix route bug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
+// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
+const apiRoutes = require('./api');
 
-router.use('/thoughts', thoughtRoutes);
-router.use('/users', userRoutes);
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
 
 module.exports = router;
