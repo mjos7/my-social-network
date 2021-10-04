d66a15ac4d8fe931550706c235fde64297dade9b
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-10-03 21:49:54 -0700

    set up models

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,69 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: [1, 'You need to say something...'],
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //   get: createdAtVal => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    // use ReactionSchema to validate data
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      min: [1, 'You need to say something...'],
+      max: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //   get: createdAtVal => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
